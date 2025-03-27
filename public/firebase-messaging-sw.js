933268b67c766094924d5036204229d8be73b6ac
Khushal Vadher <khushal@aubergine.co>
2025-03-27 16:55:49 +0530

    active check for the notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -16,11 +16,15 @@ importScripts(
     messaging.onBackgroundMessage((payload) => {
       console.log("Received background message: ", payload);
       const notificationTitle = payload.notification.title;
-      const notificationOptions = {
-        body: payload.notification.body,
-        icon: "/firebase-logo.png",
-      };
-      self.registration.showNotification(notificationTitle, notificationOptions);
+      
+      if(notificationTitle !== 'Active Check'){
+        const notificationOptions = {
+          body: payload.notification.body,
+          icon: "/firebase-logo.png",
+        };
+        self.registration.showNotification(notificationTitle, notificationOptions);
+      }
+        
     });
     
     // Handle Click on Notifications
